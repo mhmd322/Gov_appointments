e6fa0e26b66def923ecd2cff8c18bc0ac4c8142d
Mohamad Qasem <mhmd322kasem@gmail.com>
2025-08-02 14:06:26 +0400

    Add admin display for Request and Service models

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Service, Request
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']  # حسب الحقول يلي عندك
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'service', 'status', 'submitted_at']
+    list_filter = ['status', 'service']
+    search_fields = ['user__username', 'service__name']
